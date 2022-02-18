bedee182a65f81f295471414ff8d195fdacc2791
Vee <verinawasfy@gmail.com>
2022-02-18 23:49:29 +0200

    setup with components

--- BookStore/src/app/app-routing.module.ts
+++ BookStore/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllBooksComponent } from './_book/all-books/all-books.component';
+import { HomeComponent } from './_book/home/home.component';
+import { NotFoundComponent } from './_shared/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:HomeComponent,
+  },
+   {
+    path:'allBooks',
+    component:AllBooksComponent,
+  },
+     {path : "**" , component:NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- BookStore/src/app/app.module.ts
+++ BookStore/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MaterialModule } from './app.material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './_book/home/home.component';
+import { AllBooksComponent } from './_book/all-books/all-books.component';
+import { NavBarComponent } from './_shared/nav-bar/nav-bar.component';
+import { FooterComponent } from './_shared/footer/footer.component';
+import { NotFoundComponent } from './_shared/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AllBooksComponent,
+    NavBarComponent,
+    FooterComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
+    MaterialModule,
     AppRoutingModule,
     BrowserAnimationsModule
   ],
